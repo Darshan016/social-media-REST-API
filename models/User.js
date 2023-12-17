89d349f2f92c8517907555760e9c1ba6762f69b4
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-17 14:36:27 +0530

    updated User Schema

--- models/User.js
+++ models/User.js
@@ -1,41 +1,57 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    userName:{
-        type:String,
-        required:true,
-        unique:true
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
-        unique:true
+    password: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    profilePicture:{
-        type:String,
-        default:""
+    profilePicture: {
+      type: String,
+      default: "",
     },
-    coverPicture:{
-        type:String,
-        default:""
+    coverPicture: {
+      type: String,
+      default: "",
     },
-    followers:{
-        type:Array,
-        default:[]
+    followers: {
+      type: Array,
+      default: [],
     },
-    following:{
-        type:Array,
-        default:[]
+    following: {
+      type: Array,
+      default: [],
     },
-    isAdmin:{
-        type:Boolean,
-        default:false
-    }
-},{timestamps:true})
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    from: {
+      type: String,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
